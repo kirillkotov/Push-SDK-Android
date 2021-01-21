0cc67d30983123579f981c36304b5f6f7b5dc147
Redbu11 <bukovets@svyazcom.ru>
2021-01-21 16:06:27 +0700

    changed description to better display in the javadoc

--- pushsdkandroid/src/main/java/com/push/android/pushsdkandroid/PushKFirebaseService.kt
+++ pushsdkandroid/src/main/java/com/push/android/pushsdkandroid/PushKFirebaseService.kt
@@ -153,9 +153,9 @@ open class PushKFirebaseService(
     }
 
     /**
-     * Constructs the base NotificationCompat.Builder object
+     * Constructs the base NotificationCompat Builder object
      * @param data - FCM push RemoteMessage's data
-     * @return NotificationCompat.Builder object or null if message could not be read
+     * @return NotificationCompat Builder object or null if message could not be read
      */
     private fun constructNotificationBase(data: Map<String, String>): NotificationCompat.Builder? {
         //parse the data object
@@ -195,12 +195,12 @@ open class PushKFirebaseService(
     }
 
     /**
-     * Adds style to the NotificationCompat.Builder object;
+     * Adds style to the NotificationCompat Builder object;
      * You can override it to implement your own style or use a different one provided by the SDK
      *
-     * @param notificationConstruct - NotificationCompat.Builder to add style to
+     * @param notificationConstruct - NotificationCompat Builder to add style to
      * @param data - FCM push RemoteMessage's data
-     * @param notificationStyle - which built-in style to use (NotificationStyle.BIG_TEXT as default)
+     * @param notificationStyle - which built-in style to use (BIG_TEXT as default)
      */
     open fun setNotificationStyle(notificationConstruct: NotificationCompat.Builder,
                                   data: Map<String, String>,
